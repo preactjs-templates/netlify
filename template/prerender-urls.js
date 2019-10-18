8f5ed1875c97d93e5b54f8c7dc32b3171eb4f1e7
Prateek Bhatnagar <prateek89born@gmail.com>
2019-10-18 16:16:48 -0700

    fixing meta tags

--- template/prerender-urls.js
+++ template/prerender-urls.js
@@ -2,10 +2,15 @@ const { generateFileList } = require('./src/crawler');
 const { join } = require('path');
 const fs = require('fs');
 
-const [blogs, images] = generateFileList(join(__dirname, 'content')).nodes;
+const [blogs] = generateFileList(join(__dirname, 'content')).nodes;
 module.exports = () => {
 	const pages = [
-		{ url: '/' },
+		{
+			url: '/',
+			seo: {
+				cover: '/assets/profile.jpg'
+			}
+		},
 		{ url: '/contact/' },
 		{ url: '/contact/success' }
 	];
@@ -21,6 +26,7 @@ module.exports = () => {
 		const data = fs.readFileSync(join('content', 'blog', blog.id), 'utf-8').replace(/---(.*\n)*---/, '');
 		return {
 			url: `/blog/${blog.id}`,
+			seo: blog.details,
 			data: {
 				details: blog.details,
 				content: data
