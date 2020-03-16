7766e8fb3004be536e64eb1a623eb3a1e15fdd76
Prateek Bhatnagar <prateek89born@gmail.com>
2020-03-16 10:13:41 -0700

    Update preact.config.js

--- template/preact.config.js
+++ template/preact.config.js
@@ -4,7 +4,7 @@ const imageminMozjpeg = require('imagemin-mozjpeg');
 
 module.exports = (config, env) => {
 	netlifyPlugin(config);
-	env.isProd && config.plugins.push(new ImageminPlugin({
+	env.production && !env.ssr && config.plugins.push(new ImageminPlugin({
 		from: './build/assets/**',
 		pngquant: {
 			quality: '60'
